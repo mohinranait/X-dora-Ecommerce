be8347f69f2f010a81ead1d850f824e50d002937
Mohin Rana <ebrahimit49@gmail.com>
2022-12-05 02:30:09 +0600

    Product Section design done

---
+++ assets/plugins/swip-slider/swiper.res.js
@@ -0,0 +1,43 @@
+
+
+// Banner Selider js code
+var swiper = new Swiper(".bannerSlider", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+// Fiture Category js code
+var swiper = new Swiper(".fitureCategory", {
+    slidesPerView: 7,
+    spaceBetween: 20,
+    autoplay: {
+      delay: 2500,
+    },
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+// Slider Proudct js code
+var swiper = new Swiper(".productSlider", {
+    slidesPerView: 5,
+    autoplay: {
+      delay: 2500,
+    },
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
