a4ae6a9b1007a94818ef41c49bed0c033d73b7d7
Mohin Rana <ebrahimit49@gmail.com>
2022-12-06 20:57:47 +0600

    Home Page Responsive done

--- assets/plugins/swip-slider/swiper.res.js
+++ assets/plugins/swip-slider/swiper.res.js
@@ -19,6 +19,32 @@ var swiper = new Swiper(".fitureCategory", {
     },
     slidesPerGroup: 1,
     loop: true,
+    breakpoints: {
+        200:{
+            slidesPerView: 3,
+            spaceBetween: 5,
+        },
+        300:{
+            slidesPerView: 3,
+            spaceBetween: 5,
+        },
+        565:{
+            slidesPerView: 4,
+            spaceBetween: 5,
+        },
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 5,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 7,
+          spaceBetween: 20,
+        },
+      },
     loopFillGroupWithBlank: true,
     navigation: {
         nextEl: ".swiper-button-next",
@@ -35,6 +61,32 @@ var swiper = new Swiper(".productSlider", {
     },
     slidesPerGroup: 1,
     loop: true,
+    breakpoints: {
+        200:{
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        320:{
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        565:{
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 0,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 0,
+        },
+      },
     loopFillGroupWithBlank: true,
     navigation: {
         nextEl: ".swiper-button-next",
